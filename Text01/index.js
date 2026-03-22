94437aa116d3c4c35fa2140c0bf37c9530c78f5f
Ajay Gour <ajaygour369@gmail.com>
2026-03-22 20:21:00 +0530

    first commit

---
+++ Text01/index.js
@@ -0,0 +1,64 @@
+const studentId = {
+    id: 1,
+    name: "John",
+    age: 20,
+    marks: 80,
+    address: "123 Main St",
+}
+// delete studentId.id
+// console.log(studentId);
+
+// studentId.id = 121;
+// console.log(studentId);
+
+// const city = studentId.city
+// console.log(city);
+// console.log("marks" in studentId);
+// console.log("address" in studentId);
+ 
+// if("address" in studentId){
+//    studentId.address ={
+//     city: "New York",
+//     state: "NY",
+//     zip: "10001"
+//    }
+// }
+// console.log(studentId);
+
+
+
+// for (const key in studentId) {
+//     console.log(`${key} , ${studentId[key]}`);
+// }
+// console.log(studentId);
+
+const newStudent = {
+    Student1 :{
+        mane1 :"Ajay",
+        name2 : "arun" 
+    },
+    Student2 :{
+        name3 : "Varun",
+        name4 : "Tarun"
+    }
+
+}
+// console.log(newStudent);
+// const copy = newStudent.Student1
+// console.log(copy);
+
+// console.log(copy);                                   
+// console.log(newStudent.Student1);
+// const copied = {...newStudent};
+// console.log(copied);
+// copy.name2="TANYA"
+// console.log(newStudent);
+
+//Array , Object ......
+ 
+// console.log(newStudent.__proto__);
+
+
+
+
+
