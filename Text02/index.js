d7256af8e2a383955ab562186505a4ea27924265
Ajay Gour <ajaygour369@gmail.com>
2026-03-22 20:23:39 +0530

    find greater number

---
+++ Text02/index.js
@@ -0,0 +1,11 @@
+// const arr = [0,8,9,6,4,3,2];
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]>5){
+//        console.log(arr[i]);
+       
+//     }
+// }
+
+const arr= [5,2,1,7,9]
+const result = arr.filter(num=>num>5);
+console.log(result.length);
