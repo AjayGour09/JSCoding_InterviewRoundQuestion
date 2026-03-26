16b3f45649ebff3f3b118a5929e3db97b546070a
Ajay Gour <ajaygour369@gmail.com>
2026-03-26 21:08:06 +0530

    palindrome number

--- Text02/02index.js
+++ Text02/02index.js
@@ -10,3 +10,10 @@ function isPrime(n){
 console.log(isPrime(11));
 console.log(isPrime(12));
 
+function isPalindrome(str){
+    let reversed = str.split('').reverse().join('');
+    return str ===reversed;
+}
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("natvar"));
+
