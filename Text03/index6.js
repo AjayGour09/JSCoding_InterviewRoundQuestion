349ee45d94bc83d8d182c33a2edb5402af8438af
Ajay Gour <ajaygour369@gmail.com>
2026-04-08 20:33:54 +0530

    sorted Array

---
+++ Text03/index6.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5];
+
+let isSorted = true;
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+        isSorted = false;
+        break;
+    }
+}
+
+console.log(isSorted);
